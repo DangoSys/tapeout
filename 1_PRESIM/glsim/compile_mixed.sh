b7fb71d7a0a7629b8721b580cb1884de1aa0b771
SJM946 <coultrapzahnop442@gmail.com>
2026-06-08 17:20:00 +0800

    clean some scipts

--- 1_PRESIM/glsim/compile_mixed.sh
+++ 1_PRESIM/glsim/compile_mixed.sh
@@ -10,7 +10,6 @@ PATCHED_NETLIST=${PATCHED_NETLIST:-$BUILD_DIR/BuckyballAccelerator.queue_ram_zer
 SIMV=${SIMV:-$BUILD_DIR/simv_mixed}
 RANDOMIZE_RTL=${RANDOMIZE_RTL:-1}
 ZERO_RANDOM=${ZERO_RANDOM:-1}
-USE_OFFICIAL_BWP40=${USE_OFFICIAL_BWP40:-1}
 INITREG_SUPPORT=${INITREG_SUPPORT:-1}
 OFFICIAL_BWP40_TAR=${OFFICIAL_BWP40_TAR:-/data0/tsmc28/TSMC28/logic/tcbn28hpcplusbwp40p140_180b/AN61001_20180509/tcbn28hpcplusbwp40p140_110a_vlg.tar.gz}
 OFFICIAL_BWP40_MEMBER=${OFFICIAL_BWP40_MEMBER:-TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp40p140_110a/tcbn28hpcplusbwp40p140.v}
@@ -18,16 +17,9 @@ OFFICIAL_BWP40_MODEL=${OFFICIAL_BWP40_MODEL:-$BUILD_DIR/official/tcbn28hpcplusbw
 
 mkdir -p "$BUILD_DIR"
 
-if [ "$USE_OFFICIAL_BWP40" != "0" ]; then
-  mkdir -p "$(dirname "$OFFICIAL_BWP40_MODEL")"
-  tar -xOf "$OFFICIAL_BWP40_TAR" "$OFFICIAL_BWP40_MEMBER" > "$OFFICIAL_BWP40_MODEL"
-  CELL_MODEL="$OFFICIAL_BWP40_MODEL"
-else
-  python3 "$GLSIM_DIR/scripts/make_bwp40_model.py" \
-    --netlist "$NETLIST" \
-    --out "$BUILD_DIR/bwp40p140_cells.v"
-  CELL_MODEL="$BUILD_DIR/bwp40p140_cells.v"
-fi
+mkdir -p "$(dirname "$OFFICIAL_BWP40_MODEL")"
+tar -xOf "$OFFICIAL_BWP40_TAR" "$OFFICIAL_BWP40_MEMBER" > "$OFFICIAL_BWP40_MODEL"
+CELL_MODEL="$OFFICIAL_BWP40_MODEL"
 
 python3 "$GLSIM_DIR/scripts/patch_accel_queue_rams.py" \
   --in-netlist "$NETLIST" \
